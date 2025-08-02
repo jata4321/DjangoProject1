d4485c4c6c27d5eb91c06fd0444a6a6c2d2ca5af
Jakub <jata4321@gmail.com>
2025-08-02 22:04:08 +0200

    First commit

---
+++ curvecalc/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Tenor
+
+class CurveForm(forms.ModelForm):
+
+    class Meta:
+        model = Tenor
+        fields = ['type_name', 'tenor_6m', 'tenor_1y', 'tenor_2y', 'tenor_5y', 'tenor_7y', 'tenor_10y']
+
+        widgets = {
+            'type_name': forms.Select(attrs={'class': 'form-control'}),
+            'tenor_6m': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tenor_1y': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tenor_2y': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tenor_5y': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tenor_7y': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tenor_10y': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
---
+++ curvecalc/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'curvecalc'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.HomePageView.as_view(), name='home'),
+    path('listview/', views.CurveListView.as_view(), name='curve_list'),
+    path('detailview/<int:pk>/', views.CurveDetailView.as_view(), name='curve_detail'),
+    path('createview/', views.CurveCreateView.as_view(), name='curve_create'),
+    path('updateview/<int:pk>/', views.CurveUpdateView.as_view(), name='curve_update'),
+    path('deleteview/<int:pk>/', views.CurveDeleteView.as_view(), name='curve_delete'),
+]
