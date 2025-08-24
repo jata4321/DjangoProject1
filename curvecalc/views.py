8b7c089cb0c12d94d76faa6b6872cf8d8374d111
Jakub <jata4321@gmail.com>
2025-08-24 14:21:42 +0200

    Add `htmx.min.js` to the project: include the HTMX library for enhanced interactivity and AJAX capabilities in the front-end.

--- curvecalc/views.py
+++ curvecalc/views.py
@@ -1,8 +1,11 @@
 from gc import get_objects
+from re import search
 from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
-from .models import Tenor
+from pytz import country_names
+
+from .models import Tenor, Country
 from .forms import CurveForm
 from .calculations import addition, nss_curve, forward_curve
 import plotly.express as px
@@ -56,6 +59,7 @@ class CurveListView(ListView):
         queryset = super().get_queryset()
         start_date = self.request.GET.get('start_date')
         end_date = self.request.GET.get('end_date')
+        search_country = self.request.GET.get('search')
 
         if start_date:
             start_date = datetime.fromisoformat(start_date)
@@ -63,12 +67,16 @@ class CurveListView(ListView):
         if end_date:
             end_date = datetime.fromisoformat(end_date)
             queryset = queryset.filter(date__lte=end_date)
-        
+
+        if search_country:
+            queryset = queryset.filter(type_name__country__country_name__icontains=search_country)
+
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['date_form'] = DateRangeForm(self.request.GET)
+        context['search'] = self.request.GET.get('search')
         return context
 
 
